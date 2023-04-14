ac4b5691aab05b13f48ebcf40110a6a457eddd00
marianoperez.08 <marianoperez.c10@gmail.com>
2023-04-14 16:37:52 -0300

    changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,10 +3,32 @@ window.addEventListener("scroll", function() {
     var scrollPos = window.scrollY
 
     if (scrollPos > 50) {
+        navbar.style.top = "0"
         navbar.style.backgroundColor = "var(--green)"
-        navbar.style.transition = "background-color 0.2s ease-in-out";
+        navbar.style.transition = "0.5s ease-in-out";
+        navbar.style.transition = "background-color 0.3s ease-in-out";
     }
     else {
-        navbar.style.backgroundColor = "transparent";       
+        navbar.style.top = "50px"
+        navbar.style.backgroundColor = "transparent";  
+        navbar.style.transition = "0.5s ease-in-out";
+        navbar.style.transition = "background-color 0.3s ease-in-out";     
     }
 });  
+
+// SPLIDE SLIDE JS 
+
+var splide = new Splide( '.splide', {
+    perPage: 3,
+    perMove: 1,
+    omitEnd: true,
+    gap: "50px",
+    pagination: false,
+    breakpoints: {
+        576: {
+            perPage: 1,
+        }
+    }
+} );
+
+splide.mount();
